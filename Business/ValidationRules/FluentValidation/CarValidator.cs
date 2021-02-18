b33139e13082639134e09b17bc68c6396866ecc8
Mustafa Faruk Esen <mustafarukesen@gmail.com>
2021-02-18 19:23:01 +0300

    18 Şubat v2

---
+++ Business/ValidationRules/FluentValidation/CarValidator.cs
@@ -0,0 +1,26 @@
+﻿using Entities.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.ValidationRules.FluentValidation
+{
+    public class CarValidator : AbstractValidator<Car>
+    {
+        public CarValidator()
+        {
+            RuleFor(c => c.BrandId).NotEmpty();
+
+            RuleFor(c => c.ColorId).NotEmpty();
+
+            RuleFor(c => c.CarName).MinimumLength(3);
+
+            RuleFor(c => c.DailyPrice).NotEmpty();
+            RuleFor(c => c.DailyPrice).GreaterThan(100);
+            RuleFor(c => c.DailyPrice).GreaterThanOrEqualTo(150).When(c => c.BrandId == 5);
+
+            RuleFor(c => c.Description).MinimumLength(30);
+        }
+    }
+}
---
+++ Business/ValidationRules/FluentValidation/UserValidator.cs
@@ -0,0 +1,26 @@
+﻿using Entities.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.ValidationRules.FluentValidation
+{
+    public class UserValidator : AbstractValidator<User>
+    {
+        public UserValidator()
+        {
+            RuleFor(u => u.FirstName).NotEmpty();
+            RuleFor(u => u.FirstName).MaximumLength(30).NotEmpty();
+
+            RuleFor(u => u.LastName).NotEmpty();
+            RuleFor(u => u.LastName).MaximumLength(30);
+
+            RuleFor(u => u.Email).NotEmpty();
+            RuleFor(u => u.Email).MaximumLength(40);
+
+            RuleFor(u => u.Password).NotEmpty();
+            RuleFor(u => u.Password).MaximumLength(20);
+        }
+    }
+}
